19979b8da38f37076a2c296793c2efa40b0f9d98
鈴木康文 <suzukikoya@koya.local>
2017-07-26 02:46:05 +0900

    コメント追加

--- q4_ruby_2.rb
+++ q4_ruby_2.rb
@@ -1,12 +1,19 @@
-def  cutbar(m,n)
-  count = 0
-  current = 1
-  while n > current do
-    current += current < m ? current : m
-    count = count + 1
-  end
-  puts(count)
+def	cutbar(m,n)
+	count = 0
+	current = 1
+	while n > current do
+		current += current < m ? current : m
+	count = count + 1
+	end
+	puts(count)
 end
 
 cutbar(3,20)
 cutbar(5,100)
+
+# 今回のような探索処理を行う場合は、再帰を使用することが推奨されている
+# 1.優先探索
+# 　バックトラックとも呼ばれ、目的の方へ進めるだけ進んで進めなくなったら戻る
+#
+# 2.幅優先探索
+# 　探索を開始するところから近いものをリストアップし、さらにそれぞれを細かく調べていく方法
