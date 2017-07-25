b114c16be8ea7e5f958f5ec7359cbf78dd542a32
鈴木康文 <uni0360@gmail.com>
2017-07-26 02:39:43 +0900

    fix

--- q4_ruby_2.rb
+++ q4_ruby_2.rb
@@ -1,11 +1,11 @@
-def	cutbar(m,n)
-	count = 0
-	current = 1
-	while n > current do
-		current += current < m ? current : m
-		count = count + 1
-	end
-	puts(count)
+def  cutbar(m,n)
+  count = 0
+  current = 1
+  while n > current do
+    current += current < m ? current : m
+    count = count + 1
+  end
+  puts(count)
 end
 
 cutbar(3,20)
